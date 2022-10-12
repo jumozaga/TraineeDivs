e76e7188c8c287a65b68bc1ab54bf1ec0e801b83
jumozaga <jumozaga@yahoo.com>
2022-10-12 14:44:21 -0300

    Fixing screen

--- index.js
+++ index.js
@@ -20,18 +20,28 @@ let jussara,
 	joana = 0;
 
 let card1 = document.getElementById("1").addEventListener("click", () => {
-	document.getElementById("nome1").innerHTML = "VOCÊ ME ESCOLHEU !!";
+	document.getElementById("nome1").innerHTML = "VOCÊ ME ESCOLHEU ! Vamos arrasar =*";
+	document.getElementById("nome2").innerHTML = "Porquê não eu ? Eu sou ótimo, acredite !!";
+	document.getElementById("nome3").innerHTML = "A vida é feita de escolhas e você fez uma ruim !";
 	jussara++;
 });
 
 let card2 = document.getElementById("2").addEventListener("click", () => {
-	document.getElementById("nome2").innerHTML = "VOCÊ ME ESCOLHEU !!";
+	document.getElementById("nome2").innerHTML = "VOCÊ ME ESCOLHEU !! Pessoa inteligente, vamos jogar !";
+	document.getElementById("nome3").innerHTML = "Porquê não eu ? Eu sou ótima, acredite !!";
+	document.getElementById("nome1").innerHTML = "A vida é feita de escolhas e você fez uma ruim !";
 	jesse++;
 });
 
 let card3 = document.getElementById("3").addEventListener("click", () => {
 	document.getElementById("nome3").innerHTML = "VOCÊ ME ESCOLHEU !!";
+	document.getElementById("nome1").innerHTML = "Porquê não eu ? Eu sou ótima, acredite !!";
+	document.getElementById("nome2").innerHTML = "A vida é feita de escolhas e você fez uma ruim !";
 	joana++;
 });
+
+if (card1){
+	small();
+}
 
 console.log(jussara);
